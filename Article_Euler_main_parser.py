9d510835163d286b1af4ee5186a812528b42314e
Cesar Eduardo Muñoz Chavez <cesareduardomucha@hotmail.com>
2023-01-13 20:37:01 -0600

    Parser creates images 2D and 3D

--- Article_Euler_main_parser.py
+++ Article_Euler_main_parser.py
@@ -12,20 +12,24 @@ def parse_opt():
     parser = argparse.ArgumentParser()
 
     # * Add the arguments to the parser
-    parser.add_argument("--C2D", type = bool, help = "Bool 2D")
-    parser.add_argument("--save-folder2D", default = r'C:\Users\Cesar\Desktop\Codes\Python\Article_Euler_2D_and_3D\ObjectsArg\2D', 
+    parser.add_argument("--savefolder2D", default = r'ObjectsArg\2D\Images', 
                         type = str, help = "Folder")
-    parser.add_argument("--NO2D", type = int, help = "Number of objects 2D")
-    parser.add_argument("--H2D", type = int, help = "Height 2D")
-    parser.add_argument("--W2D", type = int, help = "Width 2D")
+
+    parser.add_argument("--P2D", nargs = 3, help = "Parameters 2D")
+    parser.add_argument('--C2D', action = 'store_true')
+    #parser.add_argument("--NO2D", type = int, help = "Number of objects 2D")
+    #parser.add_argument("--H2D", type = int, help = "Height 2D")
+    #parser.add_argument("--W2D", type = int, help = "Width 2D")
     
-    parser.add_argument("--C3D", type = bool, help = "Bool 3D")
-    parser.add_argument("--save-folder3D", default = r'C:\Users\Cesar\Desktop\Codes\Python\Article_Euler_2D_and_3D\ObjectsArg\3D', 
+    parser.add_argument("--savefolder3D", default = r'ObjectsArg\3D\Images', 
                         type = str, help = "Folder")
-    parser.add_argument("--NO3D", type = int, help = "Number of objects 3D")
-    parser.add_argument("--H3D", type = int, help = "Height 3D")
-    parser.add_argument("--W3D", type = int, help = "Width 3D")
-    parser.add_argument("--D3D", type = int, help = "Width 3D")
+
+    parser.add_argument("--P3D", nargs = 4, help = "Parameters 3D")
+    parser.add_argument('--C3D', action = 'store_true')
+    #parser.add_argument("--NO3D", type = int, help = "Number of objects 3D")
+    #parser.add_argument("--H3D", type = int, help = "Height 3D")
+    #parser.add_argument("--W3D", type = int, help = "Width 3D")
+    #parser.add_argument("--D3D", type = int, help = "Width 3D")
     opt = parser.parse_args()
 
     #print("{}, {}, {}".format(opt.NO2D, opt.H2D, opt.W2D))
@@ -33,14 +37,12 @@ def parse_opt():
 
     if(opt.C2D):
 
-        Images_2D = DataEuler(folder = opt.save_folder2D, NI = opt.NO2D, Height = opt.H2D, Width = opt.W2D);
-
+        Images_2D = DataEuler(folder = opt.savefolder2D, NI = opt.P2D[0], Height = opt.P2D[1], Width = opt.P2D[2]);
         Images_2D.create_data_euler_2D_random();
 
     if(opt.C3D):
 
-        Images_3D = DataEuler(folder = opt.save_folder3D, NI = opt.NO3D, Height = opt.H3D, Width = opt.W3D, Depth = opt.D3D);
-
+        Images_3D = DataEuler(folder = opt.savefolder3D, NI = opt.P3D[0], Height = opt.P3D[1], Width = opt.P3D[2], Depth = opt.P3D[3]);
         Images_3D.create_data_euler_3D_random();
 
 # ?
