be4952c10b47a1f0f8e5e14f711db0d272bdb93c
Cesar Eduardo Muñoz Chavez <cesareduardomucha@hotmail.com>
2023-02-16 19:41:25 -0600

    BinaryConverstion Class 1.0

--- Article_Euler_Number_Class_BinaryConversion.py
+++ Article_Euler_Number_Class_BinaryConversion.py
@@ -8,73 +8,75 @@ class BinaryConversion(Utilities):
     """
     A class that provides functionality to convert decimal numbers to binary and store them in a list.
 
-    Args:
-        Iter (int): The number of iterations for the binary conversion.
-        ndim (str, optional): The dimensionality of the output list. Defaults to '2D'.
+    Attributes
+    ----------
+    Iter : int
+        The number of iterations for the binary conversion.
+    ndim : str
+        The dimensionality of the output list.
 
-    Attributes:
-        Iter (int): The number of iterations for the binary conversion.
-        ndim (str): The dimensionality of the output list.
+    Methods
+    -------
+    __init__(self, Iter: int, ndim: str = '2D') -> None
+        Initializes the BinaryConversion object with the given number of iterations and dimensionality.
+    __repr__(self)
+        Returns a string representation of the BinaryConversion object.
+    __str__(self)
+        Returns a string description of the BinaryConversion object.
+    __del__(self)
+        Destroys the BinaryConversion object and prints a message to the console.
+    data_dic(self)
+        Returns a dictionary of the BinaryConversion object's attributes.
+    create_json_file(self)
+        Creates a JSON file with the BinaryConversion object's attributes and saves it to a specified file path.
+    decimal_to_binary(Decimal_value: int) -> str
+        Converts a decimal number to binary and returns the binary string.
+    decimal_to_binary_list(self, Number_iter: int, ndim: str = '2D') -> np.ndarray
+        Converts decimal numbers up to the given iteration count to binary and stores them in a list of arrays.
 
-    Methods:
-        __init__(self, Iter: int, ndim: str = '2D') -> None:
-            Initializes the BinaryConversion object with the given number of iterations and dimensionality.
-        __repr__(self):
-            Returns a string representation of the BinaryConversion object.
-        __str__(self):
-            Returns a string description of the BinaryConversion object.
-        __del__(self):
-            Destroys the BinaryConversion object and prints a message to the console.
-        data_dic(self):
-            Returns a dictionary of the BinaryConversion object's attributes.
-        create_json_file(self):
-            Creates a JSON file with the BinaryConversion object's attributes and saves it to a specified file path.
-        decimal_to_binary(Decimal_value: int) -> str:
-            Converts a decimal number to binary and returns the binary string.
-        decimal_to_binary_list(self, Number_iter: int, ndim: str = '2D') -> np.ndarray:
-            Converts decimal numbers up to the given iteration count to binary and stores them in a list of arrays.
+    Usage
+    -------
+    Instantiate the BinaryConversion object with the desired number of iterations and dimensionality.
+    Call the decimal_to_binary_list() method to convert decimal numbers up to the iteration count to binary and store
+    them in a list of arrays.
 
-    Usage:
-        Instantiate the BinaryConversion object with the desired number of iterations and dimensionality.
-        Call the decimal_to_binary_list() method to convert decimal numbers up to the iteration count to binary and store
-        them in a list of arrays.
+    Example
+    -------
+    >>> bc = BinaryConversion(5, ndim='3D')
+    >>> bc.decimal_to_binary_list(5, ndim='3D')
+    array([[[['0', '0'],
+            ['0', '1']],
 
-    Example:
-        >>> bc = BinaryConversion(5, ndim='3D')
-        >>> bc.decimal_to_binary_list(5, ndim='3D')
-        array([[[['0', '0'],
-                 ['0', '1']],
-
-                [['0', '1'],
-                 ['0', '0']]],
-
-
-               [[['0', '1'],
-                 ['0', '1']],
-
-                [['0', '1'],
-                 ['1', '0']]],
+            [['0', '1'],
+            ['0', '0']]],
 
 
-               [[['1', '0'],
-                 ['0', '0']],
+            [[['0', '1'],
+            ['0', '1']],
 
-                [['1', '0'],
-                 ['0', '1']]],
-
-
-               [[['1', '0'],
-                 ['1', '0']],
-
-                [['1', '0'],
-                 ['1', '1']]],
+            [['0', '1'],
+            ['1', '0']]],
 
 
-               [[['1', '1'],
-                 ['0', '0']],
+            [[['1', '0'],
+            ['0', '0']],
 
-                [['1', '1'],
-                 ['0', '1']]]])
+            [['1', '0'],
+            ['0', '1']]],
+
+
+            [[['1', '0'],
+            ['1', '0']],
+
+            [['1', '0'],
+            ['1', '1']]],
+
+
+            [[['1', '1'],
+            ['0', '0']],
+
+            [['1', '1'],
+            ['0', '1']]]])
     """
 
     # * Initializing (Constructor)
@@ -82,9 +84,12 @@ class BinaryConversion(Utilities):
         """
         Initialize a new instance of the BinaryConversion class.
 
-        Args:
-            Iter (int): The maximum number of iterations to perform.
-            ndim (str): The number of dimensions in the output list, either '2D' or '3D'. Defaults to '2D'.
+        Keyword Arguments
+        -----------------
+        Iter : int 
+            The maximum number of iterations to perform.
+        ndim : str 
+            The number of dimensions in the output list, either '2D' or '3D'. Defaults to '2D'.
         """
 
         # * General parameters
@@ -97,8 +102,10 @@ class BinaryConversion(Utilities):
         """
         Returns the string representation of this BinaryConversion instance.
 
-        Returns:
-            str: The string representation of this BinaryConversion instance.
+        Returns
+        -------
+        str 
+            The string representation of this BinaryConversion instance.
         """
         return f'''[{self.Iter}, 
                     {self.ndim}]''';
@@ -108,8 +115,10 @@ class BinaryConversion(Utilities):
         """
         Returns a string representation of this BinaryConversion instance.
 
-        Returns:
-            str: A string representation of this BinaryConversion instance.
+        Returns
+        -------
+        str 
+            The string representation of this BinaryConversion instance.
         """
         return  f'A class that provides functionality to convert decimal numbers to binary and store them in a list.';
     
@@ -125,8 +134,10 @@ class BinaryConversion(Utilities):
         """
         Returns a dictionary containing the instance's Iter and ndim values.
 
-        Returns:
-            dict: A dictionary containing the instance's Iter and ndim values.
+        Returns
+        -------
+        dict 
+            A dictionary containing the instance's Iter and ndim values.
         """
         return {'Iter': str(self.Iter),
                 'ndim': str(self.ndim)
@@ -136,7 +147,8 @@ class BinaryConversion(Utilities):
         """
         Creates a JSON file with the given data and saves it to the specified file path.
 
-        Returns:
+        Returns
+        -------
         None
         """
         Data = {'Iter': str(self.Iter),
@@ -151,51 +163,61 @@ class BinaryConversion(Utilities):
         """
         Convert a decimal value to its binary representation. prefix(0b)
 
-        Args:
-            Decimal_value (int): The decimal value to convert to binary.
+        Parameters
+        ----------
+        Decimal_value : str 
+            The decimal value to convert to binary.
 
-        Returns:
-            int: The binary representation of Decimal_value as an integer.
+        Returns
+        -------
+        int 
+            The binary representation of Decimal_value as an integer.
         """
 
         # * Conversion to int and binary
         Decimal_value = int(Decimal_value)
         Binary_value = format(Decimal_value, '08b')
 
-        print('\n')
-        print(Binary_value)
+        #print('\n')
+        #print(Binary_value)
 
         return Binary_value
 
     # ?
-    def decimal_to_binary_list(self, Number_iter: int, ndim: str = '2D') -> None:
+    def decimal_to_binary_list(self) -> None:
         """
         Convert a range of decimal values to binary and return them as a list.
 
-        Args:
-            Number_iter (int): The maximum decimal value to convert to binary.
-            ndim (str): The number of dimensions in the output list, either '2D' or '3D'. Defaults to '2D'.
+        Parameters
+        ----------
+        Number_iter : int
+            The maximum decimal value to convert to binary.
+        ndim : str
+            The number of dimensions in the output list, either '2D' or '3D'. Defaults to '2D'.
 
-        Returns:
-            np.ndarray: A numpy array containing the binary representations of the decimal values.
+        Returns
+        ----------
+        np.ndarray 
+            A numpy array containing the binary representations of the decimal values.
         """
         
         # * Convert the input parameter to an integer.
-        Number_iter = int(Number_iter)
+        Number_iter = int(self.Iter)
 
         # * Create an empty list to store the reshape binary numbers.
         Qs = []
 
-        if(ndim == '2D'):
+        if(self.ndim == '2D'):
             
             # * If `ndim` is '2D', create a 2D binary array for each decimal number.
             for i in range(Number_iter):
 
                 # * Convert the decimal number to binary using the `decimal_to_binary` method.
                 Binary_value = self.decimal_to_binary(i)
-
+                Binary_value = [int(x) for x in Binary_value]
+                
                 # * Reshape the resulting binary string as a 2D numpy array with dimensions (2, 2).
-                Array = np.reshape(Binary_value, (2, 2))
+                Array = np.reshape([Binary_value], (2, 2))
 
                 # * Convert the numpy array to a list and add it to the list of binary arrays.
                 Array_list = Array.tolist()
@@ -205,13 +227,14 @@ class BinaryConversion(Utilities):
             Qs = np.array(Qs)
             return Qs
         
-        elif(ndim == '3D'): 
+        elif(self.ndim  == '3D'): 
 
             # * If `ndim` is '3D', create a 3D binary array for each decimal number.     
             for i in range(Number_iter):
 
                 # * Convert the decimal number to binary using the `decimal_to_binary` method.
                 Binary_value = self.decimal_to_binary(i)
+                Binary_value = [int(x) for x in Binary_value]
 
                 # * Reshape the resulting binary string as a 3D numpy array with dimensions (2, 2, 2).
                 Array = np.reshape(Binary_value, (2, 2, 2))
